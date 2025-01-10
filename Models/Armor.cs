6a65b6015f49f1a08cebcb40d832a2036614cdb1
Amnesis <temple_wizard@msn.com>
2025-01-10 17:27:02 +0100

    Remise au propre + implémentation des coffres en jeu avec le choix aléatoire de l'arme, le matériau et les dégats

--- Models/Armor.cs
+++ Models/Armor.cs
@@ -1,49 +1,49 @@
-namespace JDR
+namespace JDR.Models
 {
 
-    public enum MaterialChest {Cloth, Leather, Wood, Metal, Gold}
+    public enum MaterialArmor {Cloth, Leather, Wood, Metal, Gold}
 
-    public abstract class Chest : Treasure
+    public class Armor
     {
         public string Name {get; set; }
-        public MaterialChest MaterialType {get; set; }
+        public MaterialArmor MaterialType {get; set; }
         public int BaseDefense {get; private set; }
         public int Weight {get; private set; }
         
         private static Random random = new Random();
 
-        public Chest (string name, MaterialChest materialType)
+        public Armor (string name, MaterialArmor materialType)
         {
             Name = name;
             MaterialType = materialType;
             SetAttributes(materialType);
         }
 
-        private void SetAttributes(MaterialChest MaterialType)
+        private void SetAttributes(MaterialArmor MaterialType)
         {
             switch (MaterialType)
             {
-                case MaterialChest.Cloth:
+                case MaterialArmor.Cloth:
                     BaseDefense = random.Next(4, 10);
                     Weight = 2;
                     break;
                 
-                case MaterialChest.Leather:
+                case MaterialArmor.Leather:
                     BaseDefense = random.Next(10, 16);
                     Weight = 4;
                     break;
 
-                case MaterialChest.Wood:
+                case MaterialArmor.Wood:
                     BaseDefense = random.Next(16, 21);
                     Weight = 6;
                     break;
                 
-                case MaterialChest.Metal:
+                case MaterialArmor.Metal:
                     BaseDefense = random.Next(21, 27);
                     Weight = 8;
                     break;
                 
-                case MaterialChest.Gold:
+                case MaterialArmor.Gold:
                     BaseDefense = random.Next(27, 32);
                     Weight = 10;
                     break;
--- Models/Treasure.cs
+++ Models/Treasure.cs
@@ -1,75 +1,41 @@
-namespace JDR
+using System;
+namespace JDR.Models
 {
-    public abstract class Treasure 
+    public class Treasure(int x, int y)
     {
+        public int X { get; set; } = x;
+        public int Y { get; set; } = y;
         private static Random random = new Random();
 
-        public WeaponsType GetMaterialTypes()
+        public static void FoundChest(int level, WeaponType weaponType, Hero hero, MaterialWeapon materialRandom, MaterialWeapon materialType)
         {
-            var weaponsType = Enum.GetValues(typeof(WeaponsType));
-
-            int randomIndex = random.Next(weaponsType.Length);
-
-            return (WeaponsType)weaponsType.GetValue(randomIndex);
+            Console.WriteLine($"Bravo {hero.Name}, tu as trouvé un coffre");
+            RandomItem(level, weaponType, hero, materialRandom, materialType);
         }
-        public MaterialWeapons MaterialChance(int level)
+       public static void RandomItem(int level, WeaponType weaponType, Hero hero, MaterialWeapon materialRandom, MaterialWeapon materialType)
         {
-            int roll = random.Next(1, 100);
+            int roll = random.Next(1,2); // 50% chance to have armor or weapon
 
-            switch(level)
+            if (roll == 1) // 50% chance to have weapon
             {
-                case 1 :
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                case 3 : 
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                case 4 : 
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                case 5 : 
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                case 6 : 
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                case >= 7 : 
-                    if (roll <=50) return MaterialWeapons.Wood;
-                    if (roll <=80) return MaterialWeapons.Stone;
-                    if (roll <=90) return MaterialWeapons.Bone;
-                    if (roll <=95) return MaterialWeapons.Metal;
-                    return MaterialWeapons.Gold;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(level), "Niveau invalide !");
+                Weapon new_weapon = new Weapon("arme", hero);
+                Console.WriteLine("Vous avez trouvé une " + new_weapon);
+            }
+            else if (roll == 2) // 50% chance to have armor
+            {
+                Console.WriteLine("Vous avez trouvé une armure qui n'est pas encore en place");
+            }
+            else
+            {
+                Console.WriteLine("Petit bug dans le programme, dsl loulou ta pas d'item 🖕");
+                return;
             }
         }
-
-        public Treasure GetRandomTreasure(int level)
-        {
-            MaterialWeapons material = MaterialChance(level);
-            WeaponsType weaponsType = GetMaterialTypes();
-            return new Weapons(material, weaponsType);
-        }
+        // public void GetRandomTreasure(int level)
+        // {
+        //     MaterialWeapon material = MaterialChance(level);
+        //     WeaponType weaponsType = GetMaterialTypes();
+        //     return new Weapon(material, weaponsType);
+        // }
     }
 }
